8922c0493062fed8f8670fa66b26bc069a095c14
Bill <me@bl-rd.dev>
2020-08-05 21:22:29 +0100

    Modify dist location and remove root level dist assets

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,31 +1,6 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const { GenerateSW } = require('workbox-webpack-plugin');
+const config = require('./webpack.config');
 
-const dist = path.resolve(__dirname);
+config.plugins.push(new GenerateSW());
 
-module.exports = {
-  mode: "production",
-  entry: {
-    index: "./js/index.js"
-  },
-  output: {
-    path: dist,
-    filename: "[name].js"
-  },
-  devServer: {
-    contentBase: dist,
-  },
-  plugins: [
-    new CopyPlugin([
-      path.resolve(__dirname, "static")
-    ]),
-
-    new WasmPackPlugin({
-      crateDirectory: __dirname,
-    }),
-
-    new GenerateSW()
-  ]
-};
+module.exports = config;
